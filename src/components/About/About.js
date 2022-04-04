e631df8da34a9c027fd84831f94214c978515042
sdshuvo98 <hellosdshuvo@gamil.com>
2022-04-04 20:42:21 +0600

    readme file and about us section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h1>About us!</h1>
+        <div className='about-us container my-5'>
+            <h3>About us !</h3>
+            <p>Welcome to Positive Energy, your number one source for all things Positive Energy Dink. We're dedicated to giving you the very best of energy drinks, with a focus on authentic, effective, energy.
+
+
+                Founded in 2002 by SD Shuvo, Positive Energy has come a long way from its beginnings in Bangladesh. When SD Shuvo first started out, his passion for "eco-friendly energy drinks" drove them to action: quit day job, do tons of research, etc. so that Positive Energy can offer you "the world's most energetic soft drink". We now serve customers all over the world, and are thrilled that we're able to turn our passion into our own website.
+
+
+                We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.</p>
+
+
+            <p className='text-center'>Sincerely,</p>
+            <h6>Positive Energy team</h6>
+
         </div>
     );
 };
